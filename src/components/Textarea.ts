1aae2d34cf931418868ead2eabd9adeaea5290f1
sandanat <sanat62@yandex.ru>
2020-12-15 16:34:47 +0300

    Реализовал textarea через наследование от BaseInput

--- src/components/Textarea.ts
+++ src/components/Textarea.ts
@@ -1,57 +1,12 @@
 import PrimeTextarea from "primevue/components/textarea/Textarea";
-import { VNodeData, VNode } from "vue";
-import { Component, Vue, Prop } from "vue-property-decorator";
+import { Component } from "vue-property-decorator";
+import BaseInput from "@/components/BaseInput";
 
 @Component({
   inheritAttrs: false
 })
-export default class Textarea extends Vue {
-  @Prop({ required: false })
-  readonly value!: string;
-
-  @Prop({ required: false })
-  readonly label!: string;
-
-  @Prop({ required: false })
-  readonly id!: string;
-
-  @Prop({ required: false })
-  readonly error?: string;
-
-  get innerId() {
-    return this.id || `textarea_${this._uid}`;
-  }
-
-  get wrapperOptions(): VNodeData {
-    return {
-      style: "display: flex; flex-direction: column"
-    };
-  }
-
-  get children(): VNode[] {
-    const children: VNode[] = [];
-
-    this.label && children.push(this.createLabel());
-    children.push(this.createTextArea());
-    this.error && children.push(this.createErrorMesage());
-
-    return children;
-  }
-
-  private createLabel() {
-    return this.$createElement(
-      "label",
-      {
-        attrs: {
-          for: this.innerId,
-          class: "p-mb-1"
-        }
-      },
-      this.label
-    );
-  }
-
-  private createTextArea() {
+export default class Textarea extends BaseInput<string> {
+  createInput() {
     return this.$createElement(PrimeTextarea, {
       props: { value: this.value },
       attrs: {
@@ -60,16 +15,6 @@ export default class Textarea extends Vue {
       },
       on: this.$listeners
     });
-  }
-
-  createErrorMesage() {
-    return this.$createElement(
-      "small",
-      {
-        class: "p-invalid"
-      },
-      this.error
-    );
   }
 
   render() {
