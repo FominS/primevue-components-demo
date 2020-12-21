bd01de98fe02899cc83cb244318cf4626a3abcb7
Fomin Sergey <fomin.sn@yandex.ru>
2020-12-21 22:31:00 +0300

    add sandbox for inputMask

--- src/views/InputMaskDemo.vue
+++ src/views/InputMaskDemo.vue
@@ -1,26 +1,73 @@
 <template>
-  <div>
-    <InputMask v-model="value" label="inputMask" mask="(999) 999-9999? x99999" placeholder="+7(999)-999-99-99"></InputMask>
+  <div class="p-grid">
+    <div class="p-col">
+      <Card>
+        <template #title>
+          InputMask
+        </template>
+        <template #content>
+          <div>
+            <InputMask
+              v-model="text"
+              :label="showLabel ? 'Phone number' : ''"
+              :error="textError"
+              :mask="mask"
+              :disabled="disabled"
+            ></InputMask>
+          </div>
+        </template>
+      </Card>
+    </div>
+    <div class="p-col">
+      <Card>
+        <template #title>
+          Options
+        </template>
+        <template #content>
+          <div class="p-field-checkbox">
+            <Checkbox id="label" v-model="showLabel" :binary="true" />
+            <label for="label">Show label</label>
+          </div>
+          <div class="p-field-checkbox">
+            <Checkbox id="label" v-model="disabled" :binary="true" />
+            <label for="label">Disabled</label>
+          </div>
+          <InputText v-model="mask" label="Mask"></InputText>
+          <hr />
+          <div class="p-field">
+            <Button @click="$v.$touch()">Проверить валидацию</Button>
+          </div>
+        </template>
+      </Card>
+    </div>
   </div>
 </template>
 
-<script>
-export default {
-  data() {
-    return {
-      value: undefined
-    };
-  },
-  methods: {
-    click() {
-      console.log("click on element");
-    },
-    focus() {
-      console.log("focus on element");
-    }
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+import { required } from "vuelidate/lib/validators";
+
+type ButtonSize = "small" | "normal" | "large";
+
+@Component({
+  validations: {
+    text: { required }
   }
-};
+})
+export default class InputTextDemo extends Vue {
+  text = "";
+  showLabel = true;
+  size: ButtonSize = "normal";
+  mask = "+7(999) 999-99-99";
+  disabled = false;
+
+  get textError() {
+    let error = "";
+    if (!this.$v.text.$dirty) return error;
+    !this.$v.text.required && (error = "Поле обязательно для заполнения");
+    return error;
+  }
+}
 </script>
 
-<style lang="scss" scoped>
-</style>
+<style lang="scss" scoped></style>
