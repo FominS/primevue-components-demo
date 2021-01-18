d2d7e5a760b645cb6a156b9cdabdaaf765e42243
sandanat <sanat62@yandex.ru>
2021-01-18 12:42:22 +0300

    Обновил primevue@2.3.0

--- src/components/AutoComplete.ts
+++ src/components/AutoComplete.ts
@@ -1,11 +1,11 @@
-import AutoComplete from "primevue/autocomplete";
+import PrimeAutoComplete from "primevue/autocomplete";
 import { Component } from "vue-property-decorator";
 import BaseInput from "@/components/BaseInput";
 
 @Component({ inheritAttrs: false })
-export default class Dropdown extends BaseInput {
+export default class AutoComplete extends BaseInput {
   createInput() {
-    return this.$createElement(AutoComplete, {
+    return this.$createElement(PrimeAutoComplete, {
       props: { value: this.value },
       scopedSlots: this.$scopedSlots,
       attrs: {
